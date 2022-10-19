8cef3dd98393024d2425c840514173affeaaf855
Thomas <thomas.ed.dick@gmail.com>
2022-10-19 15:56:00 -0600

    ad

--- app/Controllers/Reset.php
+++ app/Controllers/Reset.php
@@ -63,7 +63,7 @@ class Reset extends BaseController
 
             $passModel->addKey($user);
 
-            $link = "https://growtobe.me/reset/" . strval($num);
+            $link = "https://dev.growtobe.me/reset/" . strval($num); //TODO: remove this for officail release
 
             $send = array(
                 'from'    => 'thomasd@mail.growtobe.me',
