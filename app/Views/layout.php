587404014ec31ccdff116a966f4e8d60153dff2b
Thomas <thomas.ed.dick@gmail.com>
2022-11-03 16:34:01 -0600

    add google anaylitcs

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -25,6 +25,15 @@
     </style>
 
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-1X0JYCPTZN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-1X0JYCPTZN');
+</script>
 
 <body>
 
