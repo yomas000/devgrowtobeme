8dc198c52c8c638d97be97a9ce694c5829919a6b
Thomas <thomas.ed.dick@gmail.com>
2022-11-02 18:39:07 -0600

    new game mostly added

--- public/gamesfolder/radius-raid/js/game.js
+++ public/gamesfolder/radius-raid/js/game.js
@@ -1553,3 +1553,23 @@ window.addEventListener( 'load', function() {
 	document.documentElement.className += ' loaded';
 	$.init();
 });
+
+window.setTimeout(sendScore, 2000);
+		
+		function sendScore() {
+		  var url = window.location.href;
+		  var id = url.slice(-1);
+		  
+		  score = $.score//document.getElementById("fast_lap_time_value").innerHTML.slice(0, 4);
+		
+		  $.post("/games", {
+		
+		    "score": score,
+		    "id": id
+		
+		  }, function (data, status) {
+		    console.log(data)
+		  });
+		
+		  console.log(score);
+		}
