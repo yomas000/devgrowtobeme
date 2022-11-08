a0473e3d6211715c620e5929c31c737cc7be54e3
Thomas Dick <thomas.ed.dick@gmail.com>
2022-11-08 12:17:26 -0700

    fix over flow error

--- public/scripts.js
+++ public/scripts.js
@@ -312,11 +312,18 @@ function sendMessage(){
 }
 
 function loadLog() {
+    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
     $.ajax({
         url: "log.html",
         cache: false,
         success: function (html) {
-            $("#chatbox").html(html); //Insert chat log into the #chatbox div               
+            $("#chatbox").html(html); //Insert chat log into the #chatbox div   
+
+            //Auto-scroll           
+            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
+            if (newscrollHeight > oldscrollHeight) {
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+            }
         },
     });
 }
